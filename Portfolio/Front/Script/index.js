0835471669fcf05035bd204d340b8f1b6437252a
Jose Garcia <joseluisgarcia31000@gmail.com>
2024-01-04 18:24:15 +0100

    Maj de linterface avec le formulaire

--- Portfolio/Front/Script/index.js
+++ Portfolio/Front/Script/index.js
@@ -1,5 +1,12 @@
 const ascensor = document.querySelector('.fa-arrow-up-short-wide');
 
+const navAbout = document.querySelector('#about');
+const navServices = document.querySelector('#services');
+const navRealizations = document.querySelector('#realizations');
+const navContact = document.querySelector('#contact');
+
+
+// Control de lascenseur
 window.addEventListener('scroll', () => {
     if (scrollY > 600){
         ascensor.style.left = "96vw";
